2b01585de93b19d569ae2e57a440c59b37ff437f
BALAJI N <142398267+SparkoutBalaji@users.noreply.github.com>
2023-08-18 05:45:31 +0530

    Add files via upload

---
+++ arithmetic_operation.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Arithmetic Opersation</title>
+</head>
+<body>
+  
+  <?php
+      $a = 5;
+      $b = 3;
+
+      echo "Arithmetic Operations for number 5 & 3 <br> <br>";
+
+      $c = $a + $b;
+      echo "Addition : $c <br>";
+      
+      $c = $a - $b;
+      echo "Subtraction : $c <br>";
+
+      $c = $a * $b;
+      echo "Multiplication : $c <br>";
+
+      $c = $a / $b;
+      echo "Division : $c <br>";
+
+      $c = $a % $b;
+      echo "Modulus : $c <br>";
+
+  ?>
+
+
+</body>
+</html>
